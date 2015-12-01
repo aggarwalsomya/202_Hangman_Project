5010e2d42bc5f193d94a18528f27ae6849cac99f
aggarwalsomya <aggarwal.somya@gmail.com>
2015-12-01 14:34:50 -0800

    Comments - Observer Pattern
    Comments - Observer Pattern

--- src/com/ggl/hangman/observer/IObserver.java
+++ src/com/ggl/hangman/observer/IObserver.java
@@ -1,3 +1,6 @@
+/*Interface for the observer design pattern.
+ * Observer pattern is used to implement the logic where if a user enters a wrong guess, 
+ * hangman drawing gets build up*/
 package com.ggl.hangman.observer;
 
 public interface IObserver {
--- src/com/ggl/hangman/observer/OptionListener.java
+++ src/com/ggl/hangman/observer/OptionListener.java
@@ -14,13 +14,12 @@ public class OptionListener implements ActionListener {
 		this.menuInvoker_ = menuInvoker;
 	}
 	
-	//Here option listener is decoupled from the end observers which will do the action
-	//end observers will be the part of the command pattern.
-	//They are book and Movie in our case.
-	//Option listener simply calls the invoker to do the next action.
+	/*Here option listener is decoupled from the end observers which will do the action
+	end observers will be the part of the command pattern.
+	They are book and Movie in our case.
+	Option listener simply calls the invoker to do the next action.*/
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		System.out.println(e.getActionCommand());
 		menuInvoker_.invoke(e.getActionCommand());
 	}
